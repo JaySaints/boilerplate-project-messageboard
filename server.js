4f7da40f4092a83725c10d45720de8c68d8cf1d3
Jota <54243713+JaySaints@users.noreply.github.com>
2022-12-28 11:26:59 +0000

    Implement HelmetJS Policy

--- server.js
+++ server.js
@@ -1,35 +1,63 @@
 'use strict';
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet')
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Security Policy
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'", "https://cdn.freecodecamp.org/universal/favicons/favicon-32x32.png"],
+    scriptSrc: ["'self'", "https://code.jquery.com/jquery-2.2.1.min.js"],
+    styleSrc: ["'self'"]
+  }
+}));
+
+app.use(helmet.frameguard({
+  action: "deny"
+}));
+
+app.use(helmet.noSniff());
+
+app.use(
+  helmet.referrerPolicy({
+    policy: ["same-origin", "no-referrer-when-downgrade"],
+  })
+);
+
+app.use(
+  helmet.dnsPrefetchControl({
+    allow: false,
+  })
+);
+
 //Sample front-end
 app.route('/b/:board/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/board.html');
   });
 app.route('/b/:board/:threadid')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/thread.html');
   });
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
@@ -47,14 +75,14 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(function() {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
