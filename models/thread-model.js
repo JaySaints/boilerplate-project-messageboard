ef092b7ff1a5d79a693456ea104acd9fcf9d9500
jaysaints <pablojs2008@hotmail.com>
2022-12-29 08:35:03 -0300

    replace name shema

---
+++ models/thread-model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ThreadSchmea = mongoose.Schema({
+    board: {type: String},
+    text: {type: String},
+    delete_password: {type: String},
+    reported: {type: Boolean, default: false},
+    replies: [
+        {
+            text: {type: String},
+            delete_password : {type: String},
+            reported: {type: Boolean, default: false},     
+            created_on: {type: Date, default: Date.now}    
+            
+        }
+    ],
+},{
+    timestamps: {
+      createdAt: 'created_on', // Use `created_on` to store the created date
+      updatedAt: 'bumped_on' // and `updated_at` to store the last updated date
+    }
+});
+
+
+const threadModel = mongoose.model('Thread', ThreadSchmea);
+
+module.exports = threadModel;
