8b2b83348c139d711089c0de740d3062ffc95a8d
jaysaints <pablojs2008@hotmail.com>
2022-12-29 08:36:16 -0300

    .

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,25 @@
 'use strict';
-
+const threadController = require('../controllers/thread-controller');
+const replyController = require('../controllers/reply-controller');
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  // THREADS ROUTERS
+  app.route('/api/threads/:board').post(threadController.create);
+  
+  app.route('/api/threads/:board').get(threadController.view);
+  
+  app.route('/api/threads/:board').put(threadController.update);
+
+  app.route('/api/threads/:board').delete(threadController.destroy);
+
+  // REPLIES ROUTERS
+  app.route('/api/replies/:board').post(replyController.create);
+
+  app.route('/api/replies/:board').get(replyController.view);
+
+  app.route('/api/replies/:board').put(replyController.update);
+
+  app.route('/api/replies/:board').delete(replyController.destroy);
 
 };
