409334da88a001961253cf0246f54bfc72925b4c
jaysaints <pablojs2008@hotmail.com>
2023-01-02 09:05:26 -0300

    structure to implement 'Functional Tests'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,56 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  suite('My Tests', function () {
+    test("#1", function (done){
+      
+      done();
+    });
 
+    test("#2", function (done){
+      
+      done();
+      
+    });
+
+    test("#3", function (done){
+      
+      done();
+    });
+
+    test("#4", function (done){
+      
+      done();
+    });
+
+    test("#5", function (done){
+      
+      done();
+    });
+
+    test("#6", function (done){
+      
+      done();
+    });
+
+    test("#7", function (done){
+      
+      done();
+    });
+
+    test("#8", function (done){
+      
+      done();
+    });
+
+    test("#9", function (done){
+      
+      done();
+    });
+
+    test("#10", function (done){
+      
+      done();
+    });
+  });
 });
